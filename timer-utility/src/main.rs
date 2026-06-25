d6ef663f3a5a9ba553ff499d594057a50920692e
AnnaLWilliams <williamsab2718@gmail.com>
2026-06-25 13:41:22 -0400

    Testing to see how containers are used to store more items.  Found a better tutorial and will be trying that next but first a commit to swith devices

--- timer-utility/src/main.rs
+++ timer-utility/src/main.rs
@@ -1,3 +1,6 @@
+use iced::{Fill, Element, padding};
+use iced::widget::{Column, Text, button, column, container, text};
+
 #[derive(Default)]
 struct Counter {
     value: i64,
@@ -32,18 +35,46 @@ impl Counter {
     //}
 //}
 
-use iced::widget::{Column, button, column, text};
-
 impl Counter {
-    fn view(&self) -> Column<Message> {
-        column![
-            button("+").on_press(Message::Increment),
-            text(self.value),
-            button("-").on_press(Message::Decrement),
+    fn view(&self) -> Element<'_,Message> {
+        let CounterWidget = column![
+                button("+").on_press(Message::Increment),
+                text(self.value),
+                button("-").on_press(Message::Decrement)
         ]
+        .spacing(10);
+
+
+        container(
+            column![
+                text("Counting time"),
+                button("+").on_press(Message::Increment),
+                text(self.value),
+                button("-").on_press(Message::Decrement),
+            ]
+            .spacing(10)
+        )
+        .padding(10)
+        .center_x(Fill)
+        .center_y(Fill)
+        .into()
     }
 }
 
+//fn view(_: &()) -> Text<'_>{
+    //use iced::widget::text;
+    //use iced::{Fill, Font};
+
+    //text("Hello World\n")
+        //.font(Font::MONOSPACE)
+        //.size(30)
+        //.line_height(1.5)
+        //.width(Fill)
+        //.height(Fill)
+        //.center()
+//}
+
 pub fn main() -> iced::Result {
     iced::run(Counter::update, Counter::view)
+    //iced::run((), view)
 }
